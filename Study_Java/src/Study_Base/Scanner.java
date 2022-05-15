9b411ccd482b2b22e4c2037d5e73fa0d35e3f543
yuso <yusaei1107@gmail.com>
2022-05-15 19:59:24 +0900

    20220515

--- Study_Java/src/Study_Base/Scanner.java
+++ Study_Java/src/Study_Base/Scanner.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
 package Study_Base;
 
-public class Study_day_3rd {
+import java.util.Scanner;
+public class Scanner {
 	public static void main(String[] args) {
 		//키입력 : System.in -> 표준 입력 스트림 객체 
 		//단점 : 입력된 키를 단순한 바이트 정보로 응용프로그램에게 제공 -> 응용프로그램이 받은 바이트 정보를 문자 및 숫자로 변환해야함
 		//따라서 사용자가 원하는 타입으로 변환해주는 Scanner 클래스 사용이 효과적
 		//Scanner 사용을 위해 프로그램 맨 앞줄에 import java.util.Scanner; 삽입
-		
+		int a=1;
 	}
 }
---
+++ Study_Java/src/Study_Base/Whatif.java
@@ -0,0 +1,19 @@
+package Study_Base;
+
+import java.util.Scanner;
+public class Whatif {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String yuso=scanner.nextLine();
+		
+		if(yuso == "It's me!") {
+			System.out.println("Yuso is me!");
+		}
+		else if(yuso == "It's my friend!") {
+			System.out.println("Oh my dear!");
+		}
+		else{
+			System.out.println("Haha, you don't know me!");
+		}
+	}
+}
