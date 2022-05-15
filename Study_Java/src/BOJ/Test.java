95b08c43aa1b5862bad58a118293d9e06a149d3f
yuso <yusaei1107@gmail.com>
2022-05-15 19:28:05 +0900

    20220513~20220515

---
+++ Study_Java/src/BOJ/Test.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.util.Scanner;
+public class Test {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(a+b);
+		scanner.close();
+		}
+}
