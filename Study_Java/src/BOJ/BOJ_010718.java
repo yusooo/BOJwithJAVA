2e2f9dca376b37debae9409d7fe793389fe2346a
yuso <yusaei1107@gmail.com>
2022-05-17 16:38:13 +0900

    delete

--- Study_Java/src/BOJ/BOJ_010718.java
+++
@@ -1,8 +0,0 @@
-package BOJ;
-
-public class BOJ_010718 {
-	public static void main(String[] args) {
-		System.out.println("강한친구 대한육군");
-		System.out.println("강한친구 대한육군");
-	}
-}
--- Study_Java/src/BOJ/BOJ_2557.java
+++
@@ -1,7 +0,0 @@
-package BOJ;
-
-public class BOJ_2557 {
-	public static void main(String[] args) { //메인함수이며, 우선적으로 실행되는 메소드
-		System.out.println("Hello World"); //로그 출력
-	}
-}
