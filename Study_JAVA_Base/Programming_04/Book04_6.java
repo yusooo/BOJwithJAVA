afd07b90205015301845a5fb05f2ecc272e6a8e1
yusooo <yusaei1107@gmail.com>
2022-06-07 23:28:25 +0900

    20220607
    .

---
+++ Study_JAVA_Base/Programming_04/Book04_6.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Book04_6 {
+	double x, y;
+	int radius;
+	public Book04_6(double x, double y, int radius) {
+		x=0;
+		y=0;
+		radius=0;
+	}
+}
+
+class CircleManage{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Book04_6 c [] = new Book04_6[3];
+		for(int i=0;i<3;i++) {
+			System.out.print("x, y, radius >> ");
+			double x = sc.nextDouble();
+			double y = sc.nextDouble();
+			int radius = sc.nextInt();
+			c[i]= new Book04_6(x, y, radius);
+		}
+		int max=c[0].radius;
+		for(int i=0;i<c.length;i++) {
+			if(max<=c[i].radius) max=i;
+		}
+		System.out.println("가장 면적이 큰 원은 ("+c[max].x+","+c[max].y+")"+c[max].radius);
+		sc.close();
+	}
+}
